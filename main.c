7b70a7a0a7be50f90d395219f75fd7020f6cee6e
Dylan Leifer-Ives <dleiferives@gmail.com>
2023-09-30 00:45:43 +0000

    started work on fat32

--- main.c
+++ main.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "fat_helper.h"
 
-
-int main()
+//TODO: verify BPB_Bytes_Per_Sector * BPB_Sectors_Per_Cluster <= 1024 * 32;
+//TODO: verify sector[510] and sector[511] are equal to 0xAA
+	
+int main(int argc, char *argv[])
 {
-    FILE *fptr;
-    fptr = fopen("test1.sd","rb+");
+	FILE *fptr;
+	if(argc != 2)
+	{
+		fprintf(stderr, "No file specified, if the file does not exist one will be made.\nPlease specify like so: %s $filename$\n",argv[0]);
+		exit(0);
+	}
 
-    if(!fptr)
-    {
-        printf("file does not exist!, do you want to make one? y/n\n");
-        char c = getchar();
-        putchar(c);
-        if(c == 'n') return 0;
-        fptr = fopen("test1.sd","wb");
-        if(!fptr)
-        {
-            printf("error making file. try again\n");
-        }
-    }
+	fptr = fopen(argv[1],"rb+");
 
-    printf("file %s created\n", "test1.sd");
+	if(!fptr)
+	{
+		printf("file does not exit!\n");
+		return 0;
+		printf("file does not exist!, do you want to make one? y/n: ");
+		char c = getchar();
+		putchar(10);
+		if(c == 'n') return 0;
+		fptr = fopen(argv[1],"wb");
+		if(!fptr)
+		{
+			printf("error making file. try again\n");
+		}
+		printf("file %s created\n", argv[1]);
+	}
+	
+	// go to fat name
+	for(int i=0; i<3;i++)
+	{
+		getc(fptr);
+	}
+
+	printf("The name of this fat device: ");
+	for(int i =0; i < 8; i++)
+	{
+		putchar(getc(fptr));
+	}
+	putchar(10);
 
 
-    return 0;
+
+
+
+	return 0;
 }
